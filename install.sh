ecd4f8564ecbd8b95a36dbb6026cf7f268f4a97c
archusXIV <89104690+archusXIV@users.noreply.github.com>
2022-09-17 06:48:21 +0200

    added uninstall option

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-cp mpm /usr/local/bin/mpm
-sudo mkdir /usr/local/lib/mpm
-cp -r ./lib/* /usr/local/lib/mpm
-
-sudo chmod 755 /usr/local/lib/mpm/*
-sudo chmod 755 /usr/local/bin/mpm
-
-echo "for usage run: mpm --help"
+read -rp ' [I]nstall or [U]ninstall: ' respons
+case "$respons" in
+    i|I)    cp -f mpm /usr/local/bin/mpm
+            mkdir -p /usr/local/lib/mpm
+            cp -r -f ./lib/* /usr/local/lib/mpm
+            
+            chmod 755 /usr/local/lib/mpm/*
+            chmod 755 /usr/local/bin/mpm
+            
+            printf '%s\n' "for usage run: mpm --help"
+    ;;
+    u|U)    rm -rf /usr/local/bin/mpm
+            rm -rf /usr/local/lib/mpm
+            
+            printf '%s\n' "hope you liked it anyway..."
+    ;;
+esac
