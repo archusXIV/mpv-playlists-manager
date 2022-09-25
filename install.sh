a05b2bc00aeeb8d1238a9314baa20e188fa1269a
archusXIV <89104690+archusXIV@users.noreply.github.com>
2022-09-25 11:45:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ case "$respons" in
     i|I)    cp -f mpm /usr/local/bin/mpm
             mkdir -p /usr/local/lib/mpm
             cp -r -f ./lib/* /usr/local/lib/mpm
-            
+            mkdir -p /usr/share/licenses/mpm
+            cp -f ./LICENSE /usr/share/licenses/mpm/LICENSE
             chmod 755 /usr/local/lib/mpm/*
             chmod 755 /usr/local/bin/mpm
             
@@ -13,6 +14,7 @@ case "$respons" in
     ;;
     u|U)    rm -rf /usr/local/bin/mpm
             rm -rf /usr/local/lib/mpm
+            rm -rf /usr/share/licenses/mpm
             
             printf '%s\n' "hope you liked it anyway..."
     ;;
