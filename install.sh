89c463e8394be916a34bba5943098e78d345038c
archusXIV <89104690+archusXIV@users.noreply.github.com>
2022-09-26 16:44:43 +0200

    added conditional operators

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-read -rp ' [I]nstall or [U]ninstall: ' respons
+read -rn1 -p ' [I]nstall or [U]ninstall: ' respons
 case "$respons" in
     i|I)    cp -f mpm /usr/local/bin/mpm
-            mkdir -p /usr/local/lib/mpm
+    
+            mkdir -p /usr/local/lib/mpm && \
             cp -r -f ./lib/* /usr/local/lib/mpm
-            mkdir -p /usr/share/licenses/mpm
+            
+            mkdir -p /usr/share/licenses/mpm && \
             cp -f ./LICENSE /usr/share/licenses/mpm/LICENSE
+            
             chmod 755 /usr/local/lib/mpm/*
             chmod 755 /usr/local/bin/mpm
             
             printf '%s\n' "for usage run: mpm --help"
     ;;
-    u|U)    rm -rf /usr/local/bin/mpm
+    u|U)    rm -f /usr/local/bin/mpm
             rm -rf /usr/local/lib/mpm
             rm -rf /usr/share/licenses/mpm
             
