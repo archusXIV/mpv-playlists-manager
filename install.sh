9451c37dab4c173b357e5f0c74627a42f5d4e00d
archusXIV <89104690+archusXIV@users.noreply.github.com>
2023-05-22 10:43:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
-
-bin="/usr/local/bin"
-lib="/usr/local/lib/mpm"
+# This script will install/upgrade/remove mpm (mpv-playlists-manager).
 
 _install() {
-    cp -vf mpm "$bin"
-    mkdir -vp "$lib" && \
-    cp -vrf ./lib/* "$lib"
-            
+    cp -vf mpm /usr/local/bin
+    mkdir -vp /usr/local/lib/mpm && \
+    cp -vrf ./lib/* /usr/local/lib/mpm/
+
     mkdir -vp /usr/share/licenses/mpm && \
     cp -vf ./LICENSE /usr/share/licenses/mpm/LICENSE
-            
-    chmod 755 "$lib"/*
-    chmod 755 "$bin"/mpm
-            
+
+    chmod 755 /usr/local/lib/mpm/*
+    chmod 755 /usr/local/bin/mpm
+
     printf '%s\n' "for usage run: mpm --help"
     printf '%s\n' "install.log created."
 }
 
 _uninstall() {
-    rm -vf "$bin"/mpm
-    rm -vrf "$lib"
+    rm -vf /usr/local/bin/mpm
+    rm -vrf /usr/local/lib/mpm
     rm -vrf /usr/share/licenses/mpm
-            
+
     printf '%s\n' "hope you liked it anyway..."
     printf '%s\n' "install.log created."
 }
 
 {
 date
-printf '%s\n'
-if [ -x "$bin"/mpm ] && [ -d "$lib" ]; then
+printf '\n'
+if [ -x /usr/local/bin/mpm ] && [ -d /usr/local/lib/mpm ]; then
     read -rn 1 -p ' [R]emove or [U]pgrade mpm?: ' answer
     case "$answer" in
         r|R)
@@ -38,11 +36,11 @@ if [ -x "$bin"/mpm ] && [ -d "$lib" ]; then
         ;;
         u|U)
             if [ "$(find ./lib -type f | wc -l)" = \
-                "$(find "$lib" -type f | wc -l)" ]; then
+                "$(find /usr/local/lib/mpm -type f | wc -l)" ]; then
                 _uninstall
                 _install
             elif [ "$(find ./lib -type f | wc -l)" != \
-                "$(find "$lib" -type f | wc -l)" ]; then
+                "$(find /usr/local/lib/mpm -type f | wc -l)" ]; then
                 _uninstall
                 _install
             fi
